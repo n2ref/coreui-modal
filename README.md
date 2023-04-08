be347ea3bf38ac554de766bbf1ab937cd8043a57
shabuninil <shabuninil24@gmail.com>
2023-04-08 21:10:33 +0300

    Первый камит

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# coreui-modal
-CoreUI Modal
+# CoreUI Modal
+
+
+### Install with NPM
+
+`npm install coreui-modal`
+
+### Example usage
+
+```js
+
+CoreUI.modal.show("Title modal", "Body content", {
+    footer: "Footer content",
+    onShow: function (event) {
+        console.log('Show');
+    },
+    onShown: function (event) {
+        console.log('Shown');
+    },
+    onHide: function (event) {
+        console.log('Hide');
+    },
+    onHidden: function (event) {
+        console.log('Hidden');
+    },
+    onHidePrevented: function (event) {
+        console.log('Hide prevented');
+    }
+})
+
+```
+
+![Panel](https://raw.githubusercontent.com/shabuninil/coreui-modal/main/preview.png) 
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp       = require('gulp');
+const concat     = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify     = require('gulp-uglify');
+
+
+var conf = {
+    dist: "./dist",
+    js: {
+        file: 'coreui-modal.min.js',
+        src: [
+            'src/js/coreui.modal.js'
+        ]
+    }
+};
+
+
+
+gulp.task('build_js', function() {
+    return gulp.src(conf.js.src)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat(conf.js.file, {newLine: ";\n"}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(conf.dist));
+});
+
+gulp.task('build_js_fast', function() {
+    return gulp.src(conf.js.src)
+        .pipe(sourcemaps.init())
+        .pipe(concat(conf.js.file, {newLine: ";\n"}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(conf.dist));
+});
+
+
+gulp.task('build_watch', function() {
+    gulp.watch(conf.js.src, gulp.parallel(['build_js_fast']));
+});
+
+gulp.task("default", gulp.series([ 'build_js' ]));
